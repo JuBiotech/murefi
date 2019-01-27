1c9240ed599395be009f3c739ea88a7c1d94f993
Laura Helleckes <l.helleckes@fz-juelich.de>
2019-01-27 19:03:05 +0100

    Changed the objective to make it suitable for CDW data instead of NTU

--- murefi/murefi/objective_factory.py
+++ murefi/murefi/objective_factory.py
@@ -20,11 +20,6 @@ class ObjectiveFactory:
                 for error_model in error_models:
                     key_pred_data = error_model.key
                     if key_pred_data in data.keys():
-                        #loglikelihood with y_hat converted into y_hat-NTU
-                        if key_pred_data == 'BS':
-                            y_hat_NTU = (predicted_replicate[key_pred_data].y)/0.00885685
-                            L += error_model.evaluate_loglikelihood(y=data[key_pred_data].y, y_hat=y_hat_NTU)
-                        else:
                             L += error_model.evaluate_loglikelihood(y=data[key_pred_data].y, y_hat=predicted_replicate[key_pred_data].y)
             if numpy.isnan(L):
                 return numpy.inf
