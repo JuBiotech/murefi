3054ff680751af78a81af83a004474221cfe1670
Michael Osthege <michael.osthege@outlook.com>
2019-04-26 10:22:21 +0000

    improved observation_booleans method closes #19, #18

--- murefi/murefi/parameter_mapping.py
+++ murefi/murefi/parameter_mapping.py
@@ -1,71 +1,92 @@
+import collections
 import numpy
 import pandas
-import pathlib
 
 
 class ParameterMapping(object):
-    def __init__(self, parameters: pandas.DataFrame):
-        """ Class to map process parameters and bounds from a csv file.
+    @property
+    def order(self) -> tuple:
+        """Names of the model parameters"""
+        return self._order
         
-        The purpose of this class is to map all the parameters provided by the user as a csv file
-        to dictionaries and arrays with the user input, the extracted fitting parameters
-        and the bounds.
-        The column names (first row) of the csv files must be 'Parameter', 'S_0', 'X_0', 'mue_max', 'K_S',
-        'Y_XS', 't_lag' and 't_acc' to allow for the correct readout. For each parameter and well, the user 
-        can either type in a number if the value should be fixed or an arbitrary name if the parameter should
-        be fitted. In the column named 'Parameter', 'lower_bound' and 'upper_bound' specify the bounds for the fit.
-        The other entries in the columns specify the wells the user wishes to use for the parameter estimation (format: 'A01').
-           
-        Args:
-            parameters (pandas.DataFrame): dataframe of parameter settings.
+    @property
+    def parameters(self) -> collections.OrderedDict:
+        """Names of unique parameters in the mapping"""
+        return self._parameters
 
-        self.fitpars_bounds:  dictionary with only fitting parameters as keys and tuples (lower bound, upper bound)
-                                as value
-        self.parameters_dic:  dictionary with the values or names for all parameters provided by the user
-                                (floats and strings)
-        self.bounds_list:     list of tuples with (lower bound, upper bound) for each fitting paramter
-        self.fitpars_array:   numpy array with only the parameters for fitting
+    @property
+    def ndim(self) -> int:
+        """Dimensionality of the parameterization"""
+        return len(self.parameters)
+        
+    @property
+    def bounds(self) -> tuple:
+        """(lower, upper) tuples for all parameters"""
+        return self._bounds
+
+    @property
+    def guesses(self) -> tuple:
+        """Initial guesses for all parameters"""
+        return self._guesses
+
+    @property
+    def mapping(self) -> dict:
+        """Dictionary of parameter names or values (floats and strings) for each replicate"""
+        return self._mapping
+
+    def __init__(self, mapping:pandas.DataFrame, *, bounds:dict, guesses:dict):
+        """Helper object for mapping a global parameter vector to replicate-wise model parameters.
+        
+        The order of parameter columns will be preserved.
+        
+        Args:
+            mapping (pandas.DataFrame): dataframe of parameter settings (replicate ids in rows, parameters in columns)
+            bounds (dict): dictionary of customized bounds (unique parameter names > dimension names > None)
+            guesses (dict): dictionary of customized initial guesses (unique parameter names > dimension names > None)
         """
+        if bounds is None:
+            bounds = dict()
+        if guesses is None:
+            guesses = dict()
+        mapping = mapping.set_index(mapping.columns[0])
+
+        self._order = tuple(mapping.columns)
+
+        _parameters = {}
+        for r, id in enumerate(mapping.index):
+            for c, pname in enumerate(mapping.columns):
+                v = mapping.loc[id, pname]
+                if not str(v).replace('.', '', 1).isdigit():
+                    if v in _parameters and _parameters[v] != pname:
+                        raise ValueError(f'Unique parameter {v} is used in different model parameters.')
+                    _parameters[v] = pname
+        self._parameters = collections.OrderedDict([
+            (key, _parameters[key])
+            for key in sorted(_parameters.keys())
+        ])
         
-        if not 'Parameter' in parameters.columns:
-            raise ValueError('The parameter mapping CSV must call its first column "Parameter"')
-        parameters = parameters.set_index('Parameter')
-        parameters = parameters.T
-        
-        fitpars_bounds = {}
-        for name, column in parameters.iteritems():
-            for i, element in enumerate(column):
-                try:
-                    float(element)
-                except ValueError:
-                    fitpars_bounds[element] = (numpy.float(parameters.lower_bound[i]), numpy.float(parameters.upper_bound[i]))
-        
-        bounds_list = [
-            fitpars_bounds[parameter]
-            for parameter in fitpars_bounds.keys()
-        ]
-        
-        parameters_dic = {
-            column : parameters[column].tolist()
-            for column in parameters.columns 
-            if not 'bound' in column
+        self._bounds = tuple([
+            bounds[pkey] if pkey in bounds else
+            bounds[pdim] if pdim in bounds else
+            (None, None)
+            for pkey, pdim in self.parameters.items()
+        ])
+
+        self._guesses = tuple([
+            guesses[pkey] if pkey in guesses else
+            guesses[pdim] if pdim in guesses else
+            None
+            for pkey, pdim in self.parameters.items()
+        ])
+
+        self._mapping = {
+            key : tuple(
+                float(v) if str(v).replace('.', '', 1).isdigit() else str(v)
+                for v in [mapping.loc[key, pname] for pname in self.order]
+            )
+            for key in mapping.index
         }
-        
-        bounds_dic = {
-            column: parameters[column].tolist()
-            for column in parameters.columns
-            if 'bound' in column
-        }
-        
-        fitpars_list = [
-            parameter
-            for parameter in fitpars_bounds.keys()
-        ]
-            
-        self.fitpars_bounds = fitpars_bounds
-        self.parameters_dic = parameters_dic
-        self.bounds_list = bounds_list
-        self.fitpars_array = numpy.array(fitpars_list)
+        return
 
     def repmap(self, theta_full):
         """Remaps a full parameter vector to a dictionary of replicate-wise parameter vectors.
@@ -78,13 +99,13 @@ class ParameterMapping(object):
         """
         pname_to_pvalue = {
             pname : pvalue
-            for pname, pvalue in zip(self.fitpars_array, theta_full)
+            for pname, pvalue in zip(self.parameters, theta_full)
         }
         theta_dict = {
-            rkey : [
+            rkey : numpy.array([
                 pname_to_pvalue[pname] if isinstance(pname, str) else pname
                 for pname in pnames
-            ]
-            for rkey, pnames in self.parameters_dic.items()
+            ], dtype=float)
+            for rkey, pnames in self.mapping.items()
         }
         return theta_dict
--- murefi/tests/tests.py
+++ murefi/tests/tests.py
@@ -1,79 +1,135 @@
+import collections
 import unittest
 import numpy
 import pandas
 import pathlib
-import bletl
-from murefi import ParameterMapping, Timeseries, Replicate, Dataset
+import murefi
 
 
 class ParameterMapTest(unittest.TestCase):
-    def test_fitpars_bounds(self):
-        df = pandas.read_csv(pathlib.Path('ParTest.csv'), sep=';')
-        actual = ParameterMapping(df).fitpars_bounds
-        expected = {'test1A': (1,2), 'test1B': (1,2), 'test2C': (1,2), 'test2D': (3,4)}
-        self.assertDictEqual(actual, expected)
+    def test_init(self):
+        map_df = pandas.read_csv(r"ParTest.csv", sep=';')
+        map_df.set_index(map_df.columns[0])
+        bounds = dict(
+            S_0=(1,2),
+            X_0=(3,4),
+            mue_max=(5,6),
+            K_S=(7,8),
+            t_lag=(9,10),
+            t_acc=(11,12),
+            test1A=(1,3)
+        )
+        initial_guesses = dict(
+            S_0=0.1,
+            X_0=0.2,
+            mue_max=0.3,
+            K_S=0.4,
+            t_lag=0.5,
+            t_acc=0.6
+        )
+        parmap = murefi.ParameterMapping(map_df, bounds=bounds, guesses=initial_guesses)
+        self.assertEqual(parmap.order, ('S_0', 'X_0', 'mue_max', 'K_S', 'Y_XS', 't_lag', 't_acc'))
+        self.assertDictEqual(parmap.parameters, collections.OrderedDict([
+            ('test1A', 'S_0'),
+            ('test1B', 'X_0'),
+            ('test2C', 'mue_max'),
+            ('test2D', 'K_S')
+            ]))
+        self.assertEqual(parmap.ndim, 4)
+        self.assertEqual(parmap.bounds, ((1,3), (3,4), (5,6), (7,8)))
+        self.assertEqual(parmap.guesses, (0.1, 0.2, 0.3, 0.4))
+        self.assertEqual(parmap.mapping, {
+            'A01':('test1A', 'test1B', 3.0, 4.0, 5.0, 6.0, 7.0),
+            'B02':(11.0, 'test1B', 'test2C', 'test2D', 15.0, 16.0, 17.0)
+            })
         return
     
-    def test_fitarray(self):
-        df = pandas.read_csv(pathlib.Path('ParTest.csv'), sep=';')
-        actual = ParameterMapping(df).fitpars_array
-        expected = ['test1A', 'test1B', 'test2C', 'test2D']
-        self.assertEqual(actual.tolist(), expected)
-        return
-    
-    def test_parameters_dic(self):
-        df = pandas.read_csv(pathlib.Path('ParTest.csv'), sep=';')
-        actual = ParameterMapping(df).parameters_dic
-        expected = {'A01': ['test1A', 'test1B', '1', '1', 1, 1, 1], 'B02': ['2', 'test1B', 'test2C', 'test2D', 2, 2, 2]} 
-        self.assertDictEqual(actual, expected)
-        return
-    
-    def test_bounds_list(self):
-        df = pandas.read_csv(pathlib.Path('ParTest.csv'), sep=';')
-        actual = ParameterMapping(df).bounds_list
-        expected = [(1,2), (1,2), (1,2), (3,4)]
-        self.assertEqual(actual, expected)
+    def test_invalid_init(self):
+        map_df = pandas.read_csv(r"ParTest.csv", sep=';')
+        map_df.set_index(map_df.columns[0])
+        mapfail_df = pandas.read_csv(r"ParTestFail.csv", sep=';')
+        mapfail_df.set_index(mapfail_df.columns[0])
+        bounds = dict(
+            S_0=(1,2),
+            X_0=(3,4),
+            mue_max=(5,6),
+            K_S=(7,8),
+            t_lag=(9,10),
+            t_acc=(11,12),
+            test1A=(1,3)
+        )
+        initial_guesses = dict(
+            S_0=0.1,
+            X_0=0.2,
+            mue_max=0.3,
+            K_S=0.4,
+            t_lag=0.5,
+            t_acc=0.6
+        )
+        with self.assertRaises(TypeError):
+            _ = murefi.ParameterMapping(map_df, bounds, initial_guesses)
+        with self.assertRaises(ValueError):
+            _ = murefi.ParameterMapping(mapfail_df, bounds=bounds, guesses=initial_guesses)
+
+    def test_repmap(self):
+        map_df = pandas.read_csv(r"ParTest.csv", sep=';')
+        map_df.set_index(map_df.columns[0])
+        bounds = dict(
+            S_0=(1,2),
+            X_0=(3,4),
+            mue_max=(5,6),
+            K_S=(7,8),
+            t_lag=(9,10),
+            t_acc=(11,12),
+            test1A=(1,3)
+        )
+        initial_guesses = dict(
+            S_0=0.1,
+            X_0=0.2,
+            mue_max=0.3,
+            K_S=0.4,
+            t_lag=0.5,
+            t_acc=0.6
+        )
+        parmap = murefi.ParameterMapping(map_df, bounds=bounds, guesses=initial_guesses)
+        theta_fitted = [1,2,13,14]
+        expected = {
+            'A01': numpy.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0]),
+            'B02': numpy.array([11.0, 2.0, 13.0, 14.0, 15.0, 16.0, 17.0])
+            }
+        self.assertEqual(parmap.repmap(theta_fitted).keys(), expected.keys())
+        for key in expected.keys():
+            self.assertTrue(numpy.array_equal(parmap.repmap(theta_fitted)[key], expected[key]))
         return
 
-    
-def create_dataset_object(bletl_data, par_dic):
-    '''Function to create a dataset object for all replicates
-    Args:
-        bletl_data:     dictionary with 'FilterTimeSeries' objects (technicially 
-                        calibrated data) from bletl
-        par_dic: dictionary containing well IDs as keys and the
-                        parameters for fitting provided by the user
-    Returns: Dataset object (containing Replicate objects for each well)
-    '''
-    dataset = Dataset()
-    for well_id in par_dic.keys():
-        bs_x = bletl_data['BS10'].time[well_id]
-        bs_y = bletl_data['BS10'].value[well_id].multiply(0.1187).add(0.5866)
-        bs_ts = Timeseries('BS', list(bs_x), list(bs_y))
-        rep = Replicate(well_id)
-        rep['BS'] = bs_ts
-        dataset[well_id] = rep
-    return dataset
 
-
-class DatatypesTest(unittest.TestCase):
+class TestDataset(unittest.TestCase):
     def test_dataset(self):
-        datafile = pathlib.Path('SiLA_Coryne_Standard_20181026_150350.csv')
-        bldata = bletl.parse(datafile)
-        calibration_parameters = {
-            'cal_0': 65.91,
-            'cal_100': 40.60,
-            'phi_min': 57.45,
-            'phi_max': 18.99,
-            'pH_0': 6.46,
-            'dpH': 0.56,
-        }
-        bldata.calibrate(calibration_parameters)
-        data = bldata.calibrated_data
-        pardic = ParameterMapping(pandas.read_csv(pathlib.Path('ParTest.csv'), sep=';')).parameters_dic
-        actual = create_dataset_object(data, pardic)
-        self.assertEqual(list(actual.keys()), ['A01', 'B02'])
-        self.assertEqual(len(actual['A01']['BS']), len(data['BS10'].value['A01'])) #all data from bletl to Timeseriesin dataset
+        rep = murefi.Replicate('A01')
+        rep['S'] = murefi.Timeseries('S', [0,2,3,  5,  8  ], [1,2,3,4,5])
+        ds = murefi.Dataset()
+        ds['A01'] = rep
+        self.assertTrue('A01' in ds)
+        self.assertEqual(list(ds.keys()), ['A01'])
+        return
+
+
+class TestReplicate(unittest.TestCase):
+    def test_x_any(self):
+        rep = murefi.Replicate('A01')
+        rep['S'] = murefi.Timeseries('S', [0,2,3,  5,  8  ], [1,2,3,4,5])
+        rep['X'] = murefi.Timeseries('X', [  2,3,4,  6,8,9], [1,2,3,4,5,6])
+        self.assertTrue(numpy.array_equal(rep.x_any, [0,2,3,4,5,6,8,9]))
+        return
+
+    def test_observation_booleans(self):
+        rep = murefi.Replicate('A01')
+        rep['S'] = murefi.Timeseries('S', [0,2,3,  5,  8  ], [1,2,3,4,5])
+        rep['X'] = murefi.Timeseries('X', [  2,3,4,  6,8,9], [1,2,3,4,5,6])
+        result = rep.get_observation_booleans(['S', 'X', 'P'])
+        self.assertTrue(numpy.array_equal(result['S'], [True,True,True,False,True,False,True,False]))
+        self.assertTrue(numpy.array_equal(result['X'], [False,True,True,True,False,True,True,True]))
+        self.assertTrue(numpy.array_equal(result['P'], [False,False,False,False,False,False,False,False]))
         return
         
         
