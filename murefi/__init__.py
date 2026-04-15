164bf0be5a197955ee04e6de403c83b63292bfaa
Michael Osthege <m.osthege@fz-juelich.de>
2026-04-15 17:45:02 +0200

    Use modern build system, require Python >=3.11

--- murefi/__init__.py
+++ murefi/__init__.py
@@ -1,3 +1,22 @@
+# murefi
+# Copyright (C) 2019  Forschungszentrum Jülich GmbH
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# For more information contact the maintainers of https://github.com/JuBiotech.
+
+import importlib.metadata
+
 from . import objectives
 from .core import ParameterMapping
 from .datastructures import (
@@ -11,4 +30,4 @@ from .datastructures import (
 )
 from .ode import BaseODEModel
 
-__version__ = "5.3.0"
+__version__ = importlib.metadata.version(__package__ or __name__)
